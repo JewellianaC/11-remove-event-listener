94651e8e9c70fc4d5f4de927f2899a983e32d2bd
JewellianaC <144458061+JewellianaC@users.noreply.github.com>
2024-04-12 08:36:41 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const circle = document.querySelector('.circle')
+
+let count = 0
+let height = 0
+
+
+function toggleColor(){
+    circle.classList.toggle('white-circle')
+    count++
+    console.log(count)
+    if(count > 9){
+        circle.removeEventListener('click',toggleColor)
+    }
+}
+
+function moveCircle(){
+    height += 100
+    circle.style.top = height + 'px'
+    if (height > 300){
+        circle.removeEventListener('click',moveCircle)
+    }
+}
+
+circle.addEventListener('click',moveCircle)
+
+
+circle.addEventListener('click',toggleColor)
+
+
